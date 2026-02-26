4bf190adcbbac6d221cd6bbbd59f61c53b2da443
hiagoraian <hiagoraian98@gmail.com>
2026-02-26 08:51:38 -0300

    Controle de rotas

--- routes/web.php
+++ routes/web.php
@@ -17,52 +17,102 @@ Route::get('/login', [LoginController::class, 'show'])->name('login');
 Route::post('/login', [LoginController::class, 'store'])->name('login.store');
 Route::post('/logout', [LoginController::class, 'destroy'])->name('logout');
 
+
+/*
+|--------------------------------------------------------------------------
+| ROTAS PROFESSOR
+|--------------------------------------------------------------------------
+*/
+
 Route::middleware('auth')->group(function () {
 
+    // Dashboard Professor
     Route::get('/professor', function () {
         return view('professor.dashboard');
     })->name('professor.home');
 
-    Route::get('/admin', function () {
-        return view('admin.dashboard');
-    })->name('admin.home');
-
-    // Cidades (Admin)
-    Route::get('/admin/cidades', [CityController::class, 'index'])->name('admin.cities.index');
-    Route::post('/admin/cidades', [CityController::class, 'store'])->name('admin.cities.store');
-    Route::get('/admin/cidades/{city}/editar', [CityController::class, 'edit'])->name('admin.cities.edit');
-    Route::put('/admin/cidades/{city}', [CityController::class, 'update'])->name('admin.cities.update');
-    Route::delete('/admin/cidades/{city}', [CityController::class, 'destroy'])->name('admin.cities.destroy');
-
     // Relatórios (Professor)
-    Route::get('/professor/relatorio/{semester}', [ReportController::class, 'show'])->name('professor.report.show');
-    Route::post('/professor/relatorio/{semester}/salvar', [ReportController::class, 'save'])->name('professor.report.save');
-    Route::post('/professor/relatorio/{semester}/enviar', [ReportController::class, 'submit'])->name('professor.report.submit');
+    Route::get('/professor/relatorio/{semester}', [ReportController::class, 'show'])
+        ->name('professor.report.show');
 
-    // Situação de relatórios (Admin)
-    Route::get('/admin/relatorios', [ReportStatusController::class, 'index'])->name('admin.reports.index');
-    Route::post('/admin/relatorios/{report}/liberar-edicao', [ReportStatusController::class, 'unlock'])->name('admin.reports.unlock');
+    Route::post('/professor/relatorio/{semester}/salvar', [ReportController::class, 'save'])
+        ->name('professor.report.save');
+
+    Route::post('/professor/relatorio/{semester}/enviar', [ReportController::class, 'submit'])
+        ->name('professor.report.submit');
 
     // Alterar senha (Professor)
-    Route::get('/professor/senha', [PasswordController::class, 'edit'])->name('professor.password.edit');
-    Route::post('/professor/senha', [PasswordController::class, 'update'])->name('professor.password.update');
+    Route::get('/professor/senha', [PasswordController::class, 'edit'])
+        ->name('professor.password.edit');
 
-    // Professores (Admin)
-    Route::get('/admin/professores', [TeacherController::class, 'index'])->name('admin.teachers.index');
-    Route::get('/admin/professores/novo', [TeacherController::class, 'create'])->name('admin.teachers.create');
-    Route::post('/admin/professores', [TeacherController::class, 'store'])->name('admin.teachers.store');
-    Route::get('/admin/professores/{user}/editar', [TeacherController::class, 'edit'])->name('admin.teachers.edit');
-    Route::put('/admin/professores/{user}', [TeacherController::class, 'update'])->name('admin.teachers.update');
-    Route::delete('/admin/professores/{user}', [TeacherController::class, 'destroy'])->name('admin.teachers.destroy');
-
-    //Dashboard Admin
-    Route::get('/admin', [DashboardController::class, 'index'])->name('admin.home');
-
-    // Relatório geral (Admin)
-    Route::get('/admin/relatorio-geral', [GeneralReportController::class, 'index'])->name('admin.general-report');
-
-    Route::get('/admin/relatorios/{report}', [GeneralReportController::class, 'show'])->name('admin.reports.show');
-    
-    Route::get('/admin/relatorio-geral/pdf', [GeneralReportController::class, 'pdf'])->name('admin.general-report.pdf');
-
+    Route::post('/professor/senha', [PasswordController::class, 'update'])
+        ->name('professor.password.update');
 });
+
+
+/*
+|--------------------------------------------------------------------------
+| ROTAS ADMIN
+|--------------------------------------------------------------------------
+*/
+
+Route::prefix('admin')
+    ->middleware(['auth', 'is_admin'])
+    ->group(function () {
+
+        // Dashboard Admin
+        Route::get('/', [DashboardController::class, 'index'])
+            ->name('admin.home');
+
+        // Cidades
+        Route::get('/cidades', [CityController::class, 'index'])
+            ->name('admin.cities.index');
+
+        Route::post('/cidades', [CityController::class, 'store'])
+            ->name('admin.cities.store');
+
+        Route::get('/cidades/{city}/editar', [CityController::class, 'edit'])
+            ->name('admin.cities.edit');
+
+        Route::put('/cidades/{city}', [CityController::class, 'update'])
+            ->name('admin.cities.update');
+
+        Route::delete('/cidades/{city}', [CityController::class, 'destroy'])
+            ->name('admin.cities.destroy');
+
+        // Professores
+        Route::get('/professores', [TeacherController::class, 'index'])
+            ->name('admin.teachers.index');
+
+        Route::get('/professores/novo', [TeacherController::class, 'create'])
+            ->name('admin.teachers.create');
+
+        Route::post('/professores', [TeacherController::class, 'store'])
+            ->name('admin.teachers.store');
+
+        Route::get('/professores/{user}/editar', [TeacherController::class, 'edit'])
+            ->name('admin.teachers.edit');
+
+        Route::put('/professores/{user}', [TeacherController::class, 'update'])
+            ->name('admin.teachers.update');
+
+        Route::delete('/professores/{user}', [TeacherController::class, 'destroy'])
+            ->name('admin.teachers.destroy');
+
+        // Situação dos relatórios
+        Route::get('/relatorios', [ReportStatusController::class, 'index'])
+            ->name('admin.reports.index');
+
+        Route::post('/relatorios/{report}/liberar-edicao', [ReportStatusController::class, 'unlock'])
+            ->name('admin.reports.unlock');
+
+        // Relatório geral
+        Route::get('/relatorio-geral', [GeneralReportController::class, 'index'])
+            ->name('admin.general-report');
+
+        Route::get('/relatorio-geral/pdf', [GeneralReportController::class, 'pdf'])
+            ->name('admin.general-report.pdf');
+
+        Route::get('/relatorios/{report}', [GeneralReportController::class, 'show'])
+            ->name('admin.reports.show');
+    });
